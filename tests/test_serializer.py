310c2d6b62f3f6e10742f36f807c9d54b4a4ff30
pbtruong <truongpb2@sonat.vn>
2026-07-28 10:58:48 +0700

    [Update] Theme Level

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -54,7 +54,7 @@ def test_serializer_writes_new_pop_sort_2_format():
     assert data["mapType"] == "None"
     assert data["gameMode"] == "Classic"
     assert data["difficulty"] == "Easy"
-    assert data["themeId"] == 255
+    assert data["themeId"] == "None"
     assert data["mechanics"] == []
     assert data["time"] == 60
     assert data["piece"] == 5
